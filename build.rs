60887f029f42be3ec963b590a0d6a4cce6d11686
aster <whirlwindaster@gmail.com>
2025-08-30 10:43:55 -0400

    init commit

---
+++ build.rs
@@ -0,0 +1,24 @@
+use std::{
+    io::{self, Write},
+    process::Command,
+};
+
+fn main() {
+    println!("cargo::rerun-if-changed=client/src");
+    println!("cargo::rerun-if-changed=client/static");
+
+    let output = Command::new("npm")
+        .args(["run", "build"])
+        .current_dir("client")
+        .output()
+        .expect("failed to execute npm");
+
+    io::stdout()
+        .write_all(&output.stdout)
+        .expect("failed to write to stdout");
+    io::stderr()
+        .write_all(&output.stderr)
+        .expect("failed to write to stderr");
+
+    assert!(output.status.success(), "npm failed");
+}
---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use axum::{
+    Router, extract::Request, handler::HandlerWithoutStateExt, http::StatusCode, routing::get,
+};
+use std::net::SocketAddr;
+use tower::ServiceExt;
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env().unwrap_or_else(|_| {
+                format!("{}=debug,tower_http=debug", env!("CARGO_CRATE_NAME")).into()
+            }),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().fallback_service(ServeDir::new("client/build"));
+
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3001));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::debug!("listening on http://{}", listener.local_addr().unwrap());
+    axum::serve(listener, app.layer(TraceLayer::new_for_http()))
+        .await
+        .unwrap();
+}

db33b4d7fd084120cdd2102617864e617103d770
aster <whirlwindaster@gmail.com>
2025-08-31 22:27:22 -0400

    hook up stuff. i need to log off

--- src/game.rs
+++ src/game.rs
@@ -1,57 +1,73 @@
 use std::{collections::HashMap, sync::Arc};
 
 use axum::extract::ws::{Message, WebSocket};
-use futures_util::{SinkExt, future::join_all, stream::SplitSink};
+use bimap::BiMap;
+use futures_util::{SinkExt, future::join_all, stream::SplitSink, task::waker};
+use rand::{Rng, RngCore, SeedableRng, rng, rngs::StdRng};
 use serde::{Deserialize, Serialize};
 use thiserror::Error;
 use tokio::sync::mpsc::{self, Receiver, Sender};
 
+const TOKEN_LEN: usize = 16;
+
 #[derive(Error, Debug)]
 pub enum RoomError {
+    #[error("game already started")]
+    GameStarted,
     #[error("player '{0}' already exists")]
-    PlayerAlreadyExists(Arc<str>),
+    PlayerExists(Arc<str>),
     #[error("player '{0}' not found in room")]
     PlayerNotFound(Arc<str>),
     #[error("player '{0}' already connected")]
-    PlayerAlreadyConnected(Arc<str>),
+    PlayerConnected(Arc<str>),
     #[error("player '{0}' already disconnected")]
-    PlayerAlreadyDisconnected(Arc<str>),
+    PlayerDisconnected(Arc<str>),
 }
 
 #[derive(Debug)]
 pub struct Room {
-    channel_handle: Sender<ServerMessage>,
+    tokens: HashMap<[u8; TOKEN_LEN], Arc<str>>,
+    password: Option<Arc<str>>,
     players: HashMap<Arc<str>, Player>,
     host: Arc<str>,
-    game_state: Option<GameState>,
+    phase: Option<Phase>,
+}
+
+fn generate_token() -> [u8; TOKEN_LEN] {
+    let mut token = [0; TOKEN_LEN];
+    rng().fill_bytes(&mut token);
+    token
 }
 
 impl Room {
-    pub fn new(
-        channel_handle: Sender<ServerMessage>,
-        players: HashMap<Arc<str>, Player>,
-        host: Arc<str>,
-    ) -> Result<Self, RoomError> {
-        if !players.contains_key(&host) {
-            Err(RoomError::PlayerNotFound(host))
-        } else {
-            Ok(Self {
-                channel_handle,
-                players,
-                host,
-                game_state: None,
-            })
+    pub fn create(host: Arc<str>, password: Option<Arc<str>>) -> (Self, [u8; TOKEN_LEN]) {
+        let mut room = Self {
+            tokens: HashMap::new(),
+            password,
+            players: HashMap::new(),
+            host: host.clone(),
+            phase: None,
+        };
+
+        room.players.insert(host.clone(), Player::default());
+        let token = room.create_token(host);
+
+        (room, token)
+    }
+
+    fn create_token(&mut self, name: Arc<str>) -> [u8; TOKEN_LEN] {
+        let mut token = generate_token();
+        while self.tokens.contains_key(&token) {
+            token = generate_token();
         }
+        self.tokens.insert(token, name);
+        token
     }
 
     pub async fn handle_message(&mut self, name: Arc<str>, message: PlayerMessage) {
         match message {
             PlayerMessage::Chat { text } => self.send_all(ServerMessage::Chat { name, text }).await,
-            PlayerMessage::Start => {
-                if name == self.host && self.game_state.is_none() {
-                    self.game_state = Some(GameState::Bidding);
-                }
-            }
+            PlayerMessage::Start => unimplemented!(),
         };
     }
 
@@ -66,11 +82,29 @@ impl Room {
             .channel_handle;
 
         if channel_handle.is_some() {
-            Err(RoomError::PlayerAlreadyConnected(name))
+            Err(RoomError::PlayerConnected(name))
         } else {
             let (sender, receiver) = mpsc::channel::<Arc<ServerMessage>>(1);
-            channel_handle.insert(sender);
+            *channel_handle = Some(sender);
 
+            let _ = self
+                .send_one(
+                    name.clone(),
+                    Arc::new(ServerMessage::Welcome {
+                        name: name.clone(),
+                        players: self
+                            .players
+                            .iter()
+                            .map(|(n, p)| PlayerDescriptor {
+                                name: n.clone(),
+                                points: p.points,
+                            })
+                            .collect(),
+                        host: self.host.clone(),
+                        phase: self.phase.clone(),
+                    }),
+                )
+                .await;
             self.send_all(ServerMessage::Connect { name }).await;
             Ok(receiver)
         }
@@ -82,30 +116,44 @@ impl Room {
             .ok_or(RoomError::PlayerNotFound(name.clone()))?
             .channel_handle
             .take()
-            .ok_or(RoomError::PlayerAlreadyDisconnected(name.clone()))?;
+            .ok_or(RoomError::PlayerDisconnected(name.clone()))?;
 
         self.send_all(ServerMessage::Disconnect { name }).await;
         Ok(())
     }
 
-    pub fn join(&mut self, name: Arc<str>) -> Result<(), RoomError> {
-        if self.players.contains_key(&name) {
-            Err(RoomError::PlayerAlreadyExists(name))
+    pub fn authenticate<T>(&self, token: T) -> Option<Arc<str>>
+    where
+        T: AsRef<[u8]>,
+    {
+        self.tokens.get(token.as_ref()).cloned()
+    }
+
+    pub async fn join(&mut self, name: Arc<str>) -> Result<[u8; TOKEN_LEN], RoomError> {
+        if self.phase.is_some() {
+            Err(RoomError::GameStarted)
+        } else if self.players.contains_key(&name) {
+            Err(RoomError::PlayerExists(name))
         } else {
             self.players.insert(name.clone(), Player::default());
 
-            self.send_all(ServerMessage::Join { name });
-            Ok(())
+            self.send_all(ServerMessage::Join { name: name.clone() })
+                .await;
+            Ok(self.create_token(name))
         }
     }
 
-    pub fn leave(&mut self, name: Arc<str>) -> Result<(), RoomError> {
-        self.players
-            .remove(&name)
-            .ok_or(RoomError::PlayerNotFound(name.clone()))?;
+    pub async fn leave(&mut self, name: Arc<str>) -> Result<(), RoomError> {
+        todo!();
 
-        self.send_all(ServerMessage::Leave { name });
-        Ok(())
+        //self.players
+        //    .remove(&name)
+        //    .ok_or(RoomError::PlayerNotFound(name.clone()))?;
+        //
+        //self.tokens.remove_by_right(&name);
+        //
+        //self.send_all(ServerMessage::Leave { name }).await;
+        //Ok(())
     }
 
     async fn send_one(
@@ -119,7 +167,7 @@ impl Room {
             .ok_or(RoomError::PlayerNotFound(recipient.clone()))?
             .channel_handle
             .as_mut()
-            .ok_or(RoomError::PlayerAlreadyDisconnected(recipient))?
+            .ok_or(RoomError::PlayerDisconnected(recipient))?
             .send(message)
             .await;
         Ok(())
@@ -142,6 +190,12 @@ struct Player {
     channel_handle: Option<Sender<Arc<ServerMessage>>>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
+struct PlayerDescriptor {
+    name: Arc<str>,
+    points: i32,
+}
+
 impl Default for Player {
     fn default() -> Self {
         Player {
@@ -151,8 +205,8 @@ impl Default for Player {
     }
 }
 
-#[derive(Debug, Clone, Copy)]
-enum GameState {
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub enum Phase {
     Bidding,
 }
 
@@ -164,9 +218,26 @@ pub enum PlayerMessage {
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub enum ServerMessage {
-    Join { name: Arc<str> },
-    Leave { name: Arc<str> },
-    Connect { name: Arc<str> },
-    Disconnect { name: Arc<str> },
-    Chat { name: Arc<str>, text: Arc<str> },
+    Join {
+        name: Arc<str>,
+    },
+    Leave {
+        name: Arc<str>,
+    },
+    Connect {
+        name: Arc<str>,
+    },
+    Disconnect {
+        name: Arc<str>,
+    },
+    Welcome {
+        name: Arc<str>,
+        players: Vec<PlayerDescriptor>,
+        host: Arc<str>,
+        phase: Option<Phase>,
+    },
+    Chat {
+        name: Arc<str>,
+        text: Arc<str>,
+    },
 }
--- src/game_server.rs
+++ src/game_server.rs
@@ -1,14 +1,17 @@
 use axum::{
-    Router,
+    Json, Router,
     extract::{
         Query, State, WebSocketUpgrade,
         ws::{Message, WebSocket},
     },
-    response::IntoResponse,
+    http::{HeaderMap, StatusCode},
+    response::{IntoResponse, Response},
     routing::{get, post},
 };
 use axum_extra::extract::CookieJar;
+use base64::{Engine, engine::general_purpose::STANDARD};
 use futures_util::{SinkExt, stream::StreamExt};
+use rand::{Rng, random_range, rng, rngs::StdRng};
 use serde::{Deserialize, Serialize};
 use std::{collections::HashMap, sync::Arc};
 use thiserror::Error;
@@ -20,25 +23,128 @@ use tokio::sync::{
 
 use crate::game::{PlayerMessage, Room, ServerMessage};
 
+const NUM_CODE_CHARS: usize = 36;
+const CODE_CHARS: [char; NUM_CODE_CHARS] = [
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+    'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+];
+const CODE_LEN: usize = 4;
+
+type ServerState = Arc<Mutex<HashMap<Arc<str>, Arc<Mutex<Room>>>>>;
+
+#[derive(Error, Debug, Serialize, Clone)]
+enum ServerError {
+    #[error("room not found")]
+    RoomNotFound,
+    #[error("room code missing")]
+    MissingRoomCode,
+    #[error("token missing")]
+    MissingToken,
+    #[error("token invalid")]
+    InvalidToken,
+    #[error("room started")]
+    RoomStarted,
+}
+
+impl IntoResponse for ServerError {
+    fn into_response(self) -> Response {
+        (
+            match self {
+                Self::RoomNotFound => StatusCode::NOT_FOUND,
+                Self::MissingRoomCode => StatusCode::BAD_REQUEST,
+                Self::MissingToken => StatusCode::UNAUTHORIZED,
+                Self::InvalidToken => StatusCode::FORBIDDEN,
+                Self::RoomStarted => StatusCode::CONFLICT,
+            },
+            self,
+        )
+            .into_response()
+    }
+}
+
 pub fn init_game_server() -> Router {
     let rooms = HashMap::<Arc<str>, Arc<Mutex<Room>>>::new();
 
     Router::new()
         .route("/rooms", get(|| async {}))
+        .route("/create", post(handle_create))
         .route("/join", post(|| async {}))
         .route("/ws", get(websocket_handler))
-        .with_state(Arc::new(rooms))
+        .with_state(Arc::new(Mutex::new(rooms)))
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct CreateRequest {
+    username: Arc<str>,
+    password: Option<Arc<str>>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+struct CreateResponse {
+    code: Arc<str>,
+    token: Arc<str>,
+}
+
+fn generate_code() -> Arc<str> {
+    let mut code = String::with_capacity(CODE_LEN);
+
+    for _ in 0..CODE_LEN {
+        code.push(CODE_CHARS[rng().random_range(..NUM_CODE_CHARS)]);
+    }
+
+    code.into()
+}
+
+async fn handle_create(
+    State(rooms): State<ServerState>,
+    Json(payload): Json<CreateRequest>,
+) -> impl IntoResponse {
+    let mut code = generate_code();
+    while rooms.lock().await.contains_key(&code) {
+        code = generate_code();
+    }
+
+    let (room, host_token) = Room::create(payload.username, payload.password);
+    rooms
+        .lock()
+        .await
+        .insert(code.clone(), Arc::new(Mutex::new(room)));
+
+    Json(CreateResponse {
+        code,
+        token: STANDARD.encode(host_token).into(),
+    })
 }
 
 async fn websocket_handler(
     ws: WebSocketUpgrade,
+    headers: HeaderMap,
     Query(params): Query<HashMap<String, String>>,
-    cookies: CookieJar,
-    State(state): State<Arc<HashMap<Arc<str>, Arc<Mutex<Room>>>>>,
-) -> impl IntoResponse {
-    // TODO check token
-    let room = state.get("abcd").unwrap().clone();
-    ws.on_upgrade(|socket| websocket(socket, room, "aster".into()))
+    State(rooms): State<ServerState>,
+) -> Result<impl IntoResponse, ServerError> {
+    let room = rooms
+        .lock()
+        .await
+        .get(
+            params
+                .get("room")
+                .ok_or(ServerError::MissingRoomCode)?
+                .as_str(),
+        )
+        .ok_or(ServerError::RoomNotFound)?
+        .clone();
+
+    let name = room
+        .lock()
+        .await
+        .authenticate(
+            STANDARD
+                .decode(headers.get("token").ok_or(ServerError::MissingToken)?)
+                .map_err(|_| ServerError::InvalidToken)?,
+        )
+        .ok_or(ServerError::InvalidToken)?;
+
+    Ok(ws.on_upgrade(|socket| websocket(socket, room, name)))
 }
 
 async fn websocket(socket: WebSocket, room: Arc<Mutex<Room>>, name: Arc<str>) {
